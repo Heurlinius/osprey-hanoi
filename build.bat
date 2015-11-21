a03f93f901eefa2865e8a1c7f70724d4e19608df
Adam Heurlin <adamheurlin@gmail.com>
2015-11-21 16:58:51 +0100

    Add all source files

---
+++ build.bat
@@ -0,0 +1,13 @@
+@echo off
+
+rem Path to the compiler
+set OSPC="%OSP%\Osprey\bin\Release\Osprey.exe"
+rem Path to the library folder
+set LIB=%OSP%\lib
+
+if not exist ".\bin\" (
+	echo Creating directory .\bin
+	mkdir ".\bin"
+)
+
+%OSPC% /libpath "%LIB%" /main games.hanoi.main /out ".\bin\hanoi.ovm" /doc ".\bin\hanoi.ovm.json" /verbose main.osp
